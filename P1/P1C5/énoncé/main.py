59dda08a0c754fe683eb12c85967fec0fc5428c3
davidnadhon <davidnadhon0@gmail.com>
2024-11-27 21:40:35 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,10 @@
 # Écrivez votre code ici !
+nom="david"
+age=15
+taille=1.70
+est_etudient="vrai"
+print(f"je m'appelle {nom} j'ai {age} je fais {taille} je suis etudient {est_etudient}
+type(nom)
+type(age)
+type(taille)
+type(est_etudient)
